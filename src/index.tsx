dfc67c08dc4582afa9ae075c3badf9434581f4be
Daniil Yuzhanin <Daniil_Iuzhanin@epam.com>
2021-12-13 15:44:24 +0300

    feature: added eslint, prettier, editorconfig, husky

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
+import { CssBaseline } from '@mui/material';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-import App from './App';
+import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <CssBaseline />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
